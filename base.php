859ab7cdd8984568baf4361d4870ad227d186725
tatysouzac <47873452+tatysouzac@users.noreply.github.com>
2019-06-08 12:23:31 -0300

    Add files via upload

---
+++ base.php
@@ -0,0 +1,173 @@
+<!DOCKTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<!--titulo da página-->
+	<title> minha pagina </title>
+	<!--link do CSS-->
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<!--link para o bootstrap-->
+	<link href="public/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+	  
+	 body{
+		 overflow-x: hidden;
+	 }
+	
+    </style>
+<body>
+<!-- javascript ----------------------------------->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!------------------------------------------- Logo, barra pesquisa, carrinho, entrar cadastrar ----------------------------------------------------->
+<div>
+<header class="blog-header py-3">
+		<div class="row flex-nowrap justify-content-between align-items-right">
+			<div class="col-7 pt-0 text-left">
+				<h2>Prime Shirts</h2>
+			</div>
+			<div class="col-2 text-left">
+				<input type="text" class="form-control" placeholder="Email" >
+				<a href="#">Cadastre-se</a>
+			</div>
+			<div class="col-2 text-left">
+				<input type="text" class="form-control" placeholder="Senha" >
+				<a href="#">Esqueceu Senha</a>
+			</div>
+			<div class="col-1 ">
+				<button type="submit" class="btn btn-primary btn-block">Entrar</button>
+			</div> 
+		</div>	
+
+	</header>
+</div>
+	<!----------------------------------------   MENU ----------------------------------------->
+<div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Inicio </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Personalizar Camisetas <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Camiseta dos Criadores</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">No sei</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</nav></br>
+</div>
+</body>
+<!-------------------------------- meio da página ------------------------------------------->
+<header>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6">
+<nav class="navbar navbar-expand-lg navbar-light submenu">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="submenu collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Modelo <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cor</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Estampa</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Tamanho Camiseta
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">P</a>
+          <a class="dropdown-item" href="#">M</a>
+          <a class="dropdown-item" href="#">G</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>	
+
+
+    </div>
+    <div class="col-md-6">
+      <img src="imagens/camiseta1.png"  style="border: #000000 Solid 2px;" class="img-fluid" alt="Imagem responsiva">
+	  <br>
+	 <div align="left">
+	 <br>
+		<button type="button" class="btn btn-danger">Cancelar</button>
+		<button type="button" class="btn btn-success" style="float: right;">Comprar</button>
+    </div>
+  </div>
+</div>
+</br>
+</header>
+<!--------------------------------- rodapé -------------------------------------------------->
+<footer class="section footer-classic context-dark bg-image" style="background:#222222;">
+        <div class="container-fluid">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4"><a class="brand" href="index.html"></a>
+                <p class="color">Trabalho para FAPI, sem valor comercial.</p>
+                <p class="color rights"><span>©  </span><span class="color copyright-year">2019</span><span> </span><span>Camisetas Personalizadas</span></p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h5 class="color" >Contato</h5>
+              <dl class="color contact-list">
+                <dt>Endereço:</dt>
+                <dd>Avenida Camilo di Lellis, 1151-Pinhais/PR</dd>
+              </dl>
+              <dl class="color contact-list">
+                <dt>Email:</dt>
+                <dd><p>camisetapersonaliza@gmail.com</p></dd>
+              </dl>
+              <dl class="color contact-list">
+                <dt>Telefones:</dt>
+                <dd><p>(41) 3333-3333<p>
+                </dd>
+              </dl>
+            </div>
+            <div class="col-md-4 col-xl-2">
+              <h5 class="color">Redes Sociais</h5>
+              <ul>
+                <li><a href="#" class="fa fa-facebook"></a></li>
+                <li><a href="#" class="fa fa-twitter"></a></li>
+                <li><a href="#" class="fa fa-google"></a></li>
+				<li><a href="#" class="fa fa-linkedin"></a></li>
+				<li><a href="#" class="fa fa-instagram"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+ </footer>
+ </html>
---
+++ camisetaspersonalisadas.php
@@ -0,0 +1,207 @@
+<!DOCKTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<!--titulo da página-->
+	<title> minha pagina </title>
+	<!--link do CSS-->
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<!--link para o bootstrap-->
+	<link href="public/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+	  
+	 body{
+		 overflow-x: hidden;
+	 }
+	
+    </style>
+<body>
+<!-- javascript ----------------------------------->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!------------------------------------------- Logo, barra pesquisa, carrinho, entrar cadastrar ----------------------------------------------------->
+<div>
+
+		<div class="row flex-nowrap justify-content-between align-items-right">
+			<div class="col-10 pt-0 text-left">
+				<h2>Prime Shirts</h2>
+			</div>
+			<div class="col-2 text-right">
+			<span class="mds">Olá, nome.<span>
+				 <img src="imagens/avatar.jpg" height="80" width="80" style="border: #000000 Solid 1px;">
+				
+			</div>
+		</div>
+
+<div align="right">
+<a href="#">Sair</a>
+</div>
+</div>
+
+	<!----------------------------------------   MENU ----------------------------------------->
+<div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Inicio </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Personalizar Camisetas <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Camiseta dos Criadores</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">No sei</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</nav></br>
+</div>
+</body>
+<!-------------------------------- meio da página ------------------------------------------->
+<header>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-2">
+      <a href="#"><img src="imagens/binario.png" height="190" width="200" style="border: #000000 Solid 2px;"></a>
+    </div>
+    <div class="col-9" align="left">
+      <span>Camiseta 1</span>
+	  </br>
+	   </br>
+	    </br>
+		 </br>
+		  </br>
+	  <span>Tamanho: G</span>
+	  </br>
+	  <span>Tecido: Algodão</span>
+	  </br>
+	  <b>Preço: R$50,00</b>
+	  
+    </div>
+    <div class="col-1" align="right">
+	</br>
+		<button type="button" class="btn btn-primary btn-block">Editar</button>
+		<button type="button" class="btn btn-danger btn-block">Excluir</button>
+		<button type="button" class="btn btn-success btn-block">Comprar</button>
+  </div>
+    </div>
+	  </div>
+	  <hr/>
+	  <div class="container-fluid">
+  <div class="row">
+    <div class="col-2">
+      <a href="#"><img src="imagens/badbranca.png" height="190" width="200" style="border: #000000 Solid 2px;"></a>
+    </div>
+    <div class="col-9" align="left">
+      <span>Camiseta 2</span>
+	  </br>
+	   </br>
+	    </br>
+		 </br>
+		  </br>
+	  <span>Tamanho: G</span>
+	  </br>
+	  <span>Tecido: Algodão</span>
+	  </br>
+	  <b>Preço: R$40,00</b>
+	  
+    </div>
+    <div class="col-1" align="right">
+	</br>
+		<button type="button" class="btn btn-primary btn-block">Editar</button>
+		<button type="button" class="btn btn-danger btn-block">Excluir</button>
+		<button type="button" class="btn btn-success btn-block">Comprar</button>
+  </div>
+    </div>
+	  </div>
+	  	  <hr/>
+	  <div class="container-fluid">
+  <div class="row">
+    <div class="col-2">
+      <a href="#"><img src="imagens/sun.png" height="190" width="200" style="border: #000000 Solid 2px;"></a>
+    </div>
+    <div class="col-9" align="left">
+      <span>Camiseta 3</span>
+	  </br>
+	   </br>
+	    </br>
+		 </br>
+		  </br>
+	  <span>Tamanho: G</span>
+	  </br>
+	  <span>Tecido: Algodão</span>
+	  </br>
+	  <b>Preço: R$55,00</b>
+	  
+    </div>
+    <div class="col-1" align="right">
+	</br>
+		<button type="button" class="btn btn-primary btn-block">Editar</button>
+		<button type="button" class="btn btn-danger btn-block">Excluir</button>
+		<button type="button" class="btn btn-success btn-block">Comprar</button>
+  </div>
+    </div>
+	  </div>
+</header>
+<!--------------------------------- rodapé -------------------------------------------------->
+<footer class="section footer-classic context-dark bg-image" style="background:#222222;">
+        <div class="container-fluid">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">;
+              <div class="pr-xl-4"><a class="brand" href="index.html"></a>
+                <p class="color">Trabalho para FAPI, sem valor comercial.</p>
+                <p class="color rights"><span>©  </span><span class="color copyright-year">2019</span><span> </span><span>Camisetas Personalizadas</span></p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h5 class="color" >Contato</h5>
+              <dl class="color contact-list">
+                <dt>Endereço:</dt>
+                <dd>Avenida Camilo di Lellis, 1151-Pinhais/PR</dd>
+              </dl>
+              <dl class="color contact-list">
+                <dt>Email:</dt>
+                <dd><p>camisetapersonaliza@gmail.com</p></dd>
+              </dl>
+              <dl class="color contact-list">
+                <dt>Telefones:</dt>
+                <dd><p>(41) 3333-3333<p>
+                </dd>
+              </dl>
+            </div>
+            <div class="col-md-4 col-xl-2">
+              <h5 class="color">Redes Sociais</h5>
+              <ul>
+                <li><a href="#" class="fa fa-facebook"></a></li>
+                <li><a href="#" class="fa fa-twitter"></a></li>
+                <li><a href="#" class="fa fa-google"></a></li>
+				<li><a href="#" class="fa fa-linkedin"></a></li>
+				<li><a href="#" class="fa fa-instagram"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+ </footer>
+ </html>
